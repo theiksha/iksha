0e4fb0ff5e960fbd2e6834dc28f9e2ee88614d12
Aman Bhardwaj <abhardwaj744@gmail.com>
2025-10-06 21:02:35 +0530

    add

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: [
 		vitePreprocess(),
 		mdsvex({
-			extensions: ['.svelte', '.md', '.svx'],
-		}),
+			extensions: ['.svelte', '.md', '.svx']
+		})
 	],
 	extensions: ['.svelte', '.md', '.svx'],
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html' // 👈 this is the important line
+		}),
+		prerender: {
+			handleHttpError: 'warn' // optional, skips 404 warnings
+		}
 	}
 };
 
